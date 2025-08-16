da11046b0cd124720eadc7ec3a08dc4ba393f93c
racon121 <dogukanozdyt1@gmail.com>
2025-08-16 19:18:53 +0300

    Update server.js
    Signed-off-by: racon121 <dogukanozdyt1@gmail.com>

--- server.js
+++ server.js
@@ -11,22 +11,38 @@ let players = {};
 
 io.on("connection", socket => {
   console.log("Yeni oyuncu bağlandı:", socket.id);
-  players[socket.id] = {x:0, y:0, health:6500, currentAmmo:5, oyuncuAdi:"Oyuncu", emojiActive:false};
+  players[socket.id] = {
+    x: Math.random() * 1600,
+    y: Math.random() * 1000,
+    health: 6500,
+    currentAmmo: 3,
+    oyuncuAdi: "Oyuncu",
+    emojiActive: false
+  };
 
+  // Oyuncu durum güncellemesi
   socket.on("update", data => {
     if(players[socket.id]){
       players[socket.id] = {...players[socket.id], ...data};
     }
   });
 
-  socket.on("emoji", data=>{
-    socket.broadcast.emit("emoji", data);
+  // Emoji kullanımı
+  socket.on("emoji", () => {
+    socket.broadcast.emit("emoji", {playerId: socket.id});
   });
 
-  socket.on("disconnect", ()=>{
+  // Mermi atışı
+  socket.on("bullet", data => {
+    socket.broadcast.emit("bullet", data);
+  });
+
+  socket.on("disconnect", () => {
     delete players[socket.id];
+    io.emit("disconnect", {playerId: socket.id});
   });
 
+  // Oyuncuların durumu
   setInterval(()=>{
     socket.emit("state", players);
   },50);
