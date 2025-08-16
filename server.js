0503e25236d7a5767e6da5e4e97d2c140c6c55d8
racon121 <dogukanozdyt1@gmail.com>
2025-08-16 19:32:02 +0300

    Update server.js
    Signed-off-by: racon121 <dogukanozdyt1@gmail.com>

--- server.js
+++ server.js
@@ -1,51 +1,65 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: { origin: "*" } // Her yerden bağlanabilmesi için
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname)); // HTML, JS, PNG dosyalarını sun
+// Statik dosyaları sun
+app.use(express.static(__dirname));
 
 let players = {};
 
+// Her oyuncu bağlandığında
 io.on("connection", socket => {
   console.log("Yeni oyuncu bağlandı:", socket.id);
+
+  // Oyuncu varsayılan bilgileri
   players[socket.id] = {
-    x: Math.random() * 1600,
-    y: Math.random() * 1000,
+    x: Math.random() * 1500,
+    y: Math.random() * 900,
     health: 6500,
-    currentAmmo: 3,
+    ammo: 3,
     oyuncuAdi: "Oyuncu",
     emojiActive: false
   };
 
-  // Oyuncu durum güncellemesi
+  // Oyuncu durumu güncelle
   socket.on("update", data => {
-    if(players[socket.id]){
-      players[socket.id] = {...players[socket.id], ...data};
+    if (players[socket.id]) {
+      players[socket.id] = { ...players[socket.id], ...data };
     }
   });
 
-  // Emoji kullanımı
-  socket.on("emoji", () => {
-    socket.broadcast.emit("emoji", {playerId: socket.id});
+  // Emoji paylaşımı
+  socket.on("emoji", data => {
+    socket.broadcast.emit("emoji", data);
+    if (players[data.playerId]) players[data.playerId].emojiActive = true;
+    setTimeout(() => {
+      if (players[data.playerId]) players[data.playerId].emojiActive = false;
+    }, 5000);
   });
 
-  // Mermi atışı
-  socket.on("bullet", data => {
-    socket.broadcast.emit("bullet", data);
+  // Mermi paylaşımı (isteğe bağlı, burada sadece broadcast)
+  socket.on("bullet", bullet => {
+    socket.broadcast.emit("bullet", bullet);
   });
 
+  // Oyuncu disconnect olursa
   socket.on("disconnect", () => {
+    console.log("Oyuncu ayrıldı:", socket.id);
     delete players[socket.id];
-    io.emit("disconnect", {playerId: socket.id});
   });
 
-  // Oyuncuların durumu
-  setInterval(()=>{
+  // Her 50ms’de tüm oyunculara güncel durumu gönder
+  const interval = setInterval(() => {
     socket.emit("state", players);
-  },50);
+  }, 50);
+
+  socket.on("disconnect", () => clearInterval(interval));
 });
 
-http.listen(PORT, ()=>console.log("Server çalışıyor:", PORT));
+// Server başlat
+http.listen(PORT, () => console.log("Server çalışıyor:", PORT));
