76cdd1959ab354f5b7f8053165083dccddcc4c75
racon121 <dogukanozdyt1@gmail.com>
2025-08-16 19:48:45 +0300

    Update server.js
    Signed-off-by: racon121 <dogukanozdyt1@gmail.com>

--- server.js
+++ server.js
@@ -5,60 +5,83 @@ const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname)); // HTML, JS, PNG dosyalarını sun
+app.use(express.static(__dirname));
 
 let players = {};
 let bullets = [];
 
 io.on("connection", socket => {
   console.log("Yeni oyuncu bağlandı:", socket.id);
-  players[socket.id] = {x:100, y:100, health:6500, currentAmmo:3, oyuncuAdi:"Oyuncu", emojiActive:false};
+  players[socket.id] = {
+    x: Math.random() * 1600,
+    y: Math.random() * 1000,
+    health: 6500,
+    currentAmmo: 3,
+    oyuncuAdi: "Oyuncu",
+    emojiActive: false,
+  };
 
+  // Oyuncu pozisyon ve durum güncellemesi
   socket.on("update", data => {
-    if(players[socket.id]){
-      players[socket.id] = {...players[socket.id], ...data};
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      players[socket.id].health = data.health;
+      players[socket.id].currentAmmo = data.currentAmmo;
     }
   });
 
-  socket.on("bullet", bullet => {
-    bullets.push({...bullet, ownerId: socket.id});
+  // Mermi atma
+  socket.on("shoot", bullet => {
+    bullet.owner = socket.id;
+    bullets.push(bullet);
   });
 
+  // Emoji
   socket.on("emoji", () => {
-    socket.broadcast.emit("emoji", socket.id);
+    io.emit("emoji", socket.id);
   });
 
-  socket.on("disconnect", ()=>{
+  socket.on("disconnect", () => {
     delete players[socket.id];
-    io.emit("disconnect", socket.id);
   });
 
+  // Oyun döngüsü: her 50ms güncelle
   setInterval(() => {
-    // Mermi ve çarpışma kontrolü
-    bullets.forEach((b, i) => {
+    // Mermileri güncelle ve çarpışma kontrolü
+    bullets = bullets.filter(b => {
       b.x += b.dx;
       b.y += b.dy;
 
-      // Oyunculara çarpma
-      for(let id in players){
-        if(id !== b.ownerId){
+      // Mermiyi sınırdan sil
+      if (b.x < 0 || b.y < 0 || b.x > 1600 || b.y > 1000) return false;
+
+      // Her oyuncuya çarpma kontrolü
+      for (let id in players) {
+        if (id !== b.owner) {
           const p = players[id];
-          if(b.x > p.x && b.x < p.x+48 && b.y > p.y && b.y < p.y+48){
-            p.health -= 1250; // mermi hasarı
-            bullets.splice(i,1);
-            break;
+          if (b.x > p.x && b.x < p.x + 48 && b.y > p.y && b.y < p.y + 48) {
+            p.health -= 1250; // Hasar
+            if (p.health < 0) p.health = 0;
+            return false; // Mermi yok olur
           }
         }
       }
-
-      // Harita sınırı
-      if(b.x<0 || b.x>1600 || b.y<0 || b.y>1000){
-        bullets.splice(i,1);
-      }
+      return true;
     });
 
-    io.emit("state", {players, bullets});
+    // Oyuncu ölüme sıfırlama
+    for (let id in players) {
+      if (players[id].health <= 0) {
+        players[id].health = 6500;
+        players[id].x = Math.random() * 1600;
+        players[id].y = Math.random() * 1000;
+      }
+    }
+
+    // Durum gönder
+    io.emit("state", { players, bullets });
   }, 50);
 });
 
-http.listen(PORT, ()=>console.log("Server çalışıyor:", PORT));
+http.listen(PORT, () => console.log("Server çalışıyor:", PORT));
