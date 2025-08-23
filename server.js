1c39ced10dab6a67bc37d5580843a2a872354a48
racon121 <dogukanozdyt1@gmail.com>
2025-08-23 14:13:53 +0300

    Update server.js
    Signed-off-by: racon121 <dogukanozdyt1@gmail.com>

--- server.js
+++ server.js
@@ -1,68 +1,27 @@
-// server.js
-const io = require("socket.io")(3000, {
-  cors: { origin: "*" } // Her yerden bağlantıya izin
-});
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-let players = {}; // Tüm oyuncuların bilgisi
+app.use(express.static(__dirname));
 
 io.on("connection", socket => {
-  console.log("Yeni oyuncu bağlandı:", socket.id);
+    console.log("Yeni kullanıcı bağlandı:", socket.id);
 
-  // Yeni oyuncu ekle
-  players[socket.id] = {
-    x: Math.random()*1600,
-    y: Math.random()*1000,
-    health: 6500,
-    maxHealth: 6500,
-    currentAmmo: 3,
-    emojiActive: false,
-    name: "Oyuncu"
-  };
+    // Arkadaş daveti gönderme
+    socket.on("sendInvite", ({toId, fromName}) => {
+        io.to(toId).emit("receiveInvite", {fromName, fromId: socket.id});
+    });
 
-  // Yeni bağlanan oyuncuya mevcut state'i gönder
-  socket.emit("state", players);
-  // Diğer oyunculara yeni oyuncuyu göster
-  socket.broadcast.emit("state", players);
+    // Lobby’ye kabul etme
+    socket.on("acceptInvite", ({fromId, friendName}) => {
+        io.to(fromId).emit("inviteAccepted", {friendName});
+    });
 
-  // Pozisyon, sağlık ve ammo güncellemesi
-  socket.on("update", data => {
-    if(players[socket.id]){
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-      players[socket.id].health = data.health;
-      players[socket.id].currentAmmo = data.currentAmmo;
-    }
-    socket.broadcast.emit("state", players);
-  });
-
-  // Mermi atma
-  socket.on("bullet", b => {
-    // Diğer oyunculara mermi bilgisi gönder
-    socket.broadcast.emit("bullet", { ...b, shooterId: socket.id });
-  });
-
-  // Emoji kullanımı
-  socket.on("emoji", () => {
-    if(players[socket.id]){
-      players[socket.id].emojiActive = true;
-      socket.broadcast.emit("emoji", { playerId: socket.id });
-
-      // 5 saniye sonra emoji kapanır
-      setTimeout(()=>{
-        if(players[socket.id]){
-          players[socket.id].emojiActive = false;
-          socket.broadcast.emit("emoji", { playerId: socket.id });
-        }
-      }, 5000);
-    }
-  });
-
-  // Oyuncu ayrıldığında
-  socket.on("disconnect", () => {
-    console.log("Oyuncu ayrıldı:", socket.id);
-    delete players[socket.id];
-    socket.broadcast.emit("state", players);
-  });
+    // Lobby’ye katılım bildirimi
+    socket.on("joinLobby", ({name}) => {
+        socket.broadcast.emit("lobbyUpdate", {name});
+    });
 });
 
-console.log("Server çalışıyor! Port: 3000");
+http.listen(3000, () => console.log("Server 3000 portunda çalışıyor"));
